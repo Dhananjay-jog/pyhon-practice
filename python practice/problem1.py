ed7b79c275ee2d4f9ef934e067a8a54eb8327d5f
Dhananjay Dinesh Jog <168922027+Dhananjay-jog@users.noreply.github.com>
2024-11-05 00:27:46 +0530

    chapter1
    chapter 1

---
+++ python practice/problem1.py
@@ -0,0 +1,11 @@
+# this '''.....''' is use to print mutipal lines in program
+print('''this is first program
+      omtklbiorbioirb'a
+      robjparijbhrg9[0hq
+      argjvnriognhrhew
+      rgjiowergjwrhgwj[r
+      rgrjgiwr[jgoiwjrpgj
+      
+      grgiohrwiohriogh[orhig
+      rigjirghwhrg[hioirhgwioahroihw
+      grwihowpgiowrhiogawener''')
