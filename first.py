ea0c3292da29ec7c60aa050f650583209cdd2000
Dhananjay Dinesh Jog <168922027+Dhananjay-jog@users.noreply.github.com>
2024-11-05 01:09:52 +0530

    pyjokes
    pyjokes

---
+++ first.py
@@ -0,0 +1,9 @@
+import pyjokes
+print("printing jokes....")
+joke = pyjokes.get_joke()
+print(joke)
+""" this is my program"""
+# this s single line comment
+# iohioahrhioahiov;
+# ribhauiriaiono
+# ririobiona
